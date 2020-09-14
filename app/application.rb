a40ffc3aa70358781f094899bb32ed7fc21b4ba3
sdonohue0918 <sdonohue0918@gmail.com>
2020-09-14 05:43:34 -0400

    all rspec passing

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -13,6 +14,24 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.any?
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      else 
+        resp.write "Your cart is empty"
+      end
+    elsif req.path.match(/add/)
+      c_item = req.params["item"]
+
+      if @@items.include?(c_item)
+        @@cart << c_item
+        resp.write "added #{c_item}"
+      else
+        resp.write "We don't have that item"
+      end
+
     else
       resp.write "Path Not Found"
     end
@@ -27,4 +46,15 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
+
+  # def add(item_name)
+  #   if @@items.include?(item_name)
+  #     @@cart << item_name
+  #   else
+  #     return "We don't have that item"
+  #   end
+    
+  # end
+
+
 end
